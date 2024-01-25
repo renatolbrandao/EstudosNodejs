710ced8a6019371fe5c99532abdcffbb92fa2268
Renato Lima Brandão <renatolbrandao@gmail.com>
2024-01-25 17:02:51 -0300

    add Rocketseat - Projeto IMC

---
+++ Stage 05/calcula-imc/js/script.js
@@ -0,0 +1,36 @@
+import { Modal } from './modal.js'
+import { AlertError } from './alert-error.js'
+import { IMC, notANumber} from './utils.js'
+
+// variaveis - variables
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+
+form.onsubmit = (event) =>{
+    event.preventDefault()
+    
+    const weight = inputWeight.value
+    const height = inputHeight.value
+    
+    const showAlertError = notANumber(weight) || notANumber(height)
+
+    if (showAlertError){
+        AlertError.open()
+        return;
+    }else{
+        AlertError.close()
+    }
+
+    const result = IMC(weight, height)
+    const message = `Seu IMC é de  ${result}`
+    
+    Modal.message.innerText = message
+    Modal.open()
+
+}
+
+inputWeight.oninput = () => AlertError.close()
+inputHeight.oninput = () => AlertError.close()
+
